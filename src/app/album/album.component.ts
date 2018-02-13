d6a920402ec5cd6c99043dafd85d22013505ca13
Lee Dumond <leedumond@gmail.com>
2018-02-13 09:27:10 -0600

    implement album component

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { SpotifyService } from '../spotify.service';
 
 @Component({
   selector: 'app-album',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album.component.css']
 })
 export class AlbumComponent implements OnInit {
+  id: string;
+  album: Object;
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    private route: ActivatedRoute,
+    private spotifyService: SpotifyService
+  ) {
+    route.params.subscribe(params => {
+      this.id = params['id'];
+    });
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.spotifyService
+      .getAlbum(this.id)
+      .subscribe((res: any) => {
+        this.renderAlbum(res);
+      });
+  }
+
+  renderAlbum(res: any): void {
+    this.album = res;
+  }
+
+  back(): void {
+    this.location.back();
   }
 
 }
